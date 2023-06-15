dc8aa463a3ad1b0ee17598d710fc157115c03f1d
vy285 <vy.lt280502@gmail.com>
2023-06-15 22:06:08 +0700

    hoan thanh navigate productitem

--- src/components/products/ProductItem.jsx
+++ src/components/products/ProductItem.jsx
@@ -1,63 +1,18 @@
-// import React from "react";
-// import { FaStar } from "react-icons/fa";
-// import { Link } from "react-router-dom";
-
-// const ProductItem = ({ product }) => {
-// 	const endPointImg =
-// 		process.env.REACT_APP_END_POINT_IMAGE || "http://localhost:4001";
-
-// 	return (
-// 		<Link
-// 			onClick={() => {
-// 				window.scrollTo({ top: 0, behavior: "smooth" });
-// 			}}
-// 			to={`/product/${product.id}`}
-// 			className="col-4 m-3 sm-12">
-// 			<div className="productContainer">
-// 				<div className="sp">
-// 					<img
-// 						className="anhSp"
-// 						src={
-// 							product?.images && product.images.length > 0
-// 								? `${endPointImg}/${product.images[0].path}`
-// 								: ""
-// 						}
-// 						alt="normal"
-// 					/>
-// 					<div className="thong_tin">
-// 						<p className="description">{product.description}</p>
-// 						<p className="sp__cost">${product.price}</p>
-// 						<div>
-// 							<p className="rate">
-// 								<div class="reviews">
-// 									<FaStar color="yellow" />
-// 									<FaStar color="yellow" />
-// 									<FaStar color="yellow" />
-// 									<FaStar />
-// 								</div>
-// 							</p>
-// 						</div>
-// 					</div>
-// 				</div>
-// 			</div>
-// 			day la muc can chinh sua
-// 		</Link>
-// 	);
-// };
-
-// export default ProductItem;
 
 import React from "react";
 import { Link } from "react-router-dom";
 import {FaStar} from "react-icons/fa"
 import {IoIosAdd , IoIosCart} from "react-icons/io"
 import "./ProductItem.css"
-
+import { addToCart } from "../../redux/cartSlice";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const ProductItem = ({ product }) => {
-
+	const dispatch = useDispatch();
+	
 	const YesStar = [];
-	// console.log(Math.floor(product.ratings));
+	// console.log(product.ratings);
 
 	for (let i = 0; i < Math.floor(3); i++) {
 		YesStar.push(<FaStar color="yellow" />);
@@ -69,20 +24,39 @@ const ProductItem = ({ product }) => {
 	}
 
 
+	const navigate = useNavigate();
+	const handleAddToCart = (event) => {
+		event.stopPropagation();
+		dispatch(addToCart(product));
+		// window.location.href = '/products'
+		navigate('/products',{replace:true});
+		console.log('thuc hien navigate')
+	};
+
+	const handleBuyNow = (event) => {
+		event.stopPropagation();
+		dispatch(addToCart(product));
+		// window.location.href = '/cart'
+		navigate("/order",{replace:true});
+		console.log("day la buy now");
+	};
+
+
 	const endPointImg =
 		process.env.REACT_APP_END_POINT_IMAGE || "http://localhost:4001";
 
 	return (
-		<Link
+		<div
 			onClick={() => {
 				window.scrollTo({ top: 0, behavior: "smooth" });
+				navigate('/product/${product.id}');  
 			}}
-			to={`/product/${product.id}`}
+			// to={`/product/${product.id}`}
 			className="col-4 m-3 sm-12"
 		>
 
 			<div className="pi-product">
-				<Link to="#" className="pi-img-prod">
+				<Link to={`/product/${product.id}`} className="pi-img-prod">
 					<img className="pi-img-fluid" src={
 						product?.images && product.images.length > 0
 							? `${endPointImg}/${product.images[0].path}`
@@ -124,15 +98,15 @@ const ProductItem = ({ product }) => {
 						<p>{product.seller}</p>
 					</div>
 					<p className="pi-bottom-area">
-						<Link to={""} className="pi-add-cart pi-cho">ADD TO CART<IoIosAdd /></Link>
-						<Link to={''} className="pi-buy-now pi-cho">BUY NOW<IoIosCart /></Link>
+						<button onClick={handleAddToCart}  className="pi-add-cart pi-cho">ADD TO CART<IoIosAdd /></button>
+						<button  onClick={handleBuyNow} className="pi-buy-now pi-cho">BUY NOW<IoIosCart /></button>
 					</p>
 				</div>
 
 			</div>
 
 
-		</Link>
+		</div>
 	)
 }
 
